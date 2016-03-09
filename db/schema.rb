3d59b0cf2714a0f65da790437302fad3d249c727
Nick <nick@nicksit.com>
2016-03-08 16:17:41 -0800

    changed over to postgres database from sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160308171617) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "entrypoints", force: :cascade do |t|
     t.integer  "location"
     t.integer  "status"
